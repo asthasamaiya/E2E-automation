9d6c4ad9317f9cf01220a0fe6b8b002d544444ac
asthasamaiya <47205387+asthasamaiya@users.noreply.github.com>
2021-07-23 13:36:36 +0530

    Add files via upload

---
+++ Academy/src/test/java/Qaclick/Academy/AppTest.java
@@ -0,0 +1,19 @@
+package test.java.Qaclick.Academy;
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
